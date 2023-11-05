245ea6a57384f7603dd76db7a54764b7d7fa93b7
T--Roar <dennartempest@aol.com>
2023-11-05 01:10:17 -0700

    scanning  commit

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = jsondecode({"access_token": "${var.google_access_token}"})
+  credentials = jsondecode("{\"access_token\": \"${var.google_access_token}\"}")
   project     = var.project_id
   region      = var.region
 }
