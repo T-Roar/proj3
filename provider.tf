3d9ff248be080e195d04ca64db051eec094bdf39
T--Roar <dennartempest@aol.com>
2023-11-05 19:31:21 -0700

    shine  commit

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "google" {
-  credentials = file("${path.module}/terraform/key.json")
+  credentials = file("${path.module}/key.json")
   project     = var.gcp_project_id
   region      = var.gcp_region
 }
+
