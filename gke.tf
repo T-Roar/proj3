822db0b37b394077c502a3704db2e36b09c62e58
T--Roar <dennartempest@aol.com>
2023-11-05 20:07:06 -0700

    shine  commit

--- gke.tf
+++ gke.tf
@@ -3,6 +3,6 @@ resource "google_container_cluster" "my_cluster" {
   name     = "my-cluster"
   location = "us-central1"
 
-  network    = google_compute_network.my_network.self_link
+  network    = google_compute_network.roar_network.self_link
   subnetwork = google_compute_subnetwork.my_subnet[0].self_link # Use the first subnet for the nodes
 }
--- net.tf
+++ net.tf
@@ -1,6 +1,6 @@
 # Create a VPC network
-resource "google_compute_network" "my_network" {
-  name                    = "my-network"
+resource "google_compute_network" "roar_network" {
+  name                    = "roar-network"
   auto_create_subnetworks = false
 }
 
@@ -8,7 +8,7 @@ resource "google_compute_network" "my_network" {
 resource "google_compute_subnetwork" "my_subnet" {
   for_each      = { for i in range(var.subnet_count) : i => i }
   name          = "subnet-${each.value}"
-  network       = google_compute_network.my_network.self_link
+  network       = google_compute_network.roar_network.self_link
   ip_cidr_range = "10.0.${each.value}.0/24"
   region        = "us-central1"
 }
@@ -16,7 +16,7 @@ resource "google_compute_subnetwork" "my_subnet" {
 # Create a firewall rule to allow incoming traffic
 resource "google_compute_firewall" "allow_ingress" {
   name    = "allow-ingress"
-  network = google_compute_network.my_network.self_link
+  network = google_compute_network.roar_network.self_link
 
   allow {
     protocol = "tcp"
