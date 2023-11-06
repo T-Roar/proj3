88467b5738e1654053236c4952da6d55e4438612
T--Roar <dennartempest@aol.com>
2023-11-05 21:08:46 -0700

    shine  commit

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 # Output cluster information
 output "cluster_endpoint" {
-  value = google_container_cluster.my_cluster.endpoint
+  value = google_container_cluster.roar_cluster.endpoint
 }
 
 output "cluster_master_version" {
-  value = google_container_cluster.my_cluster.master_version
+  value = google_container_cluster.roar_cluster.master_version
 }
